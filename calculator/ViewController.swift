89ffc64f567bc7eddd321961e320436952140ad0
王蕾 <867550109@qq.com>
2018-10-19 20:06:31 +0800

    改变

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var z: UITextField!
+    
+    @IBAction func caculate(_ sender: Any) {
+        var a = 0
+        a = Int(x.text!)!
+        var b = 0
+        b = Int(y.text!)!
+        var c = 0
+        c = a + b
+        z.text = "\(c)"
+    }
+    override func viewDidLoad()  {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        x.text = "0"
+        y.text = "0"
+        z.text = "0"
     }
-
-
 }
-
